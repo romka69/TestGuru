c608a2f7258b7cf282bfe3c9d6c1ec42e4513efe
roman <romka69tver@gmail.com>
2018-12-15 00:06:02 +0300

    now migration reversible

--- db/migrate/20181213204932_add_default_values.rb
+++ db/migrate/20181213204932_add_default_values.rb
@@ -1,7 +1,7 @@
 class AddDefaultValues < ActiveRecord::Migration[5.2]
   def change
-    change_column_default(:tests, :level, 0)
+    change_column_default(:tests, :level, from: nil, to: 0)
 
-    change_column_default(:answers, :correct, false)
+    change_column_default(:answers, :correct, from: nil, to: false)
   end
 end
