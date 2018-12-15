9a5368b769d4c52532773c94a42bb620243f4c05
roman <romka69tver@gmail.com>
2018-12-16 00:08:09 +0300

    add def 'list_tests_in_category'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.list_tests_in_category(name_category, sort = :desc)
+    joins(:category).where(categories: { title: name_category }).order(title: sort).pluck(:title)
+  end
 end
