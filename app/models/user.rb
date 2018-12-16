abe4115450e53882a083d21e8ab84df8b6a5f3d9
roman <romka69tver@gmail.com>
2018-12-16 22:42:54 +0300

    rename two methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results
 
-  def list_tests(level)
+  def test_by_level(level)
     tests.where(level: level)
   end
 end
