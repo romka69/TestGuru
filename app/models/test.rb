17d246128e3e4c1c7b29b1e125985c271ec90273
roman <romka69tver@gmail.com>
2018-12-17 23:18:18 +0300

    update associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+
+  has_many :results
+  has_many :users, through: :results
+
+  belongs_to :author, class_name: "User"
 
   def self.sort_by_name_category(name_category, sort = :desc)
     joins(:category).where(categories: { title: name_category }).order(title: sort).pluck(:title)
