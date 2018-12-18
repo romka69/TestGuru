6d64bae0dacb55cbd12446c2e42b4df5cdcb4891
roman <romka69tver@gmail.com>
2018-12-19 01:52:29 +0300

    adds ':dependent'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  has_many :questions, dependent: :destroy
 
-  has_many :results
+  has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   belongs_to :author, class_name: "User"
