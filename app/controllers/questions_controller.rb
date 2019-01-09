0fa12dcd653941141ac630760b311242f9921399
roman <romka69tver@gmail.com>
2019-01-10 00:04:40 +0300

    fix 'create' on 'new' on create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,7 +20,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = @test.questions.create(question_params)
+    question = @test.questions.new(question_params)
 
     if question.save
       render plain: "'#{question.body}' was created"
