6d14e0eb79d8d4d85d3c980faf59a9e8caddf060
roman <romka69tver@gmail.com>
2019-01-14 00:30:15 +0300

    update routes for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :answers
   root 'tests#index'
 
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
-
 
 end
