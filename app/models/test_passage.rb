7d977ba7fa2e1949ff16886097a7a5baa1403c23
roman <romka69tver@gmail.com>
2019-01-15 00:13:07 +0300

    adds more functional in test_passages. but 'next'  has broken

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,42 +1,54 @@
 class TestPassage < ApplicationRecord
+
+  SUCCESS_SCORE = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_question, on: :create
 
   def completed?
     current_question.nil?
   end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    self.current_question = next_question
     save!
+  end
+
+  def test_success_done?
+    success_progress >= SUCCESS_SCORE
+  end
+
+  def success_progress
+    (correct_questions.to_f / test.questions.count.to_f) * 100
+  end
+
+  def current_question_index
+    test.questions.order(:id).where('id < ?', current_question.id).count + 1
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_question
+    if current_question.nil? && test.present?
+      self.current_question = test.questions.first
+    else
+      self.current_question = test.questions
+                                  .order(:id)
+                                  .where('id > ?', current_question.id)
+                                  .first
+    end
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-        correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.true_answer
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
 end
