4a5cc6801fc023637b99f1f50f126ed3f1d0fb44
roman <romka69tver@gmail.com>
2019-01-14 03:40:36 +0300

    fix correct_questions

--- db/migrate/20190113213843_add_cur_question_and_cor_question_to_results.rb
+++ db/migrate/20190113213843_add_cur_question_and_cor_question_to_results.rb
@@ -1,6 +1,6 @@
 class AddCurQuestionAndCorQuestionToResults < ActiveRecord::Migration[5.2]
   def change
     add_reference(:results, :current_question, foreign_key: true)
-    add_column(:results, :correct_question, :integer, default: 0)
+    add_column(:results, :correct_questions, :integer, default: 0)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_01_13_232403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "current_question_id"
-    t.integer "correct_question", default: 0
+    t.integer "correct_questions", default: 0
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
