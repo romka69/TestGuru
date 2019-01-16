bd38ecf4927af569bf8b6c9ab661bf5621e0cfd4
roman <romka69tver@gmail.com>
2019-01-16 22:54:57 +0300

    changed database on postgre and bundler update

--- db/migrate/20190113213843_add_cur_question_and_cor_question_to_results.rb
+++ db/migrate/20190113213843_add_cur_question_and_cor_question_to_results.rb
@@ -1,6 +1,6 @@
 class AddCurQuestionAndCorQuestionToResults < ActiveRecord::Migration[5.2]
   def change
-    add_reference(:results, :current_question, foreign_key: true)
+    add_reference(:results, :current_question, foreign_key: { to_table: :questions })
     add_column(:results, :correct_questions, :integer, default: 0)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_01_13_232403) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "question_id"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -29,18 +32,18 @@ ActiveRecord::Schema.define(version: 2019_01_13_232403) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "test_id"
+    t.bigint "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "test_passages", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "current_question_id"
+    t.bigint "current_question_id"
     t.integer "correct_questions", default: 0
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
@@ -50,10 +53,10 @@ ActiveRecord::Schema.define(version: 2019_01_13_232403) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "category_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
@@ -66,4 +69,11 @@ ActiveRecord::Schema.define(version: 2019_01_13_232403) do
     t.string "email", null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
