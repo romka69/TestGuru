ea22da0811adc5e3853dd46c30903d8048e89a6e
roman <romka69tver@gmail.com>
2019-01-22 00:03:57 +0300

    adds '/login' with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get 'user/new'
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
