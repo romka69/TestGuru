3f471d8cce9ed7fc72ab3860bb42fe855c3a8b02
roman <romka69tver@gmail.com>
2019-01-22 00:34:00 +0300

    add flash notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
+  helper_method :current_user,
+                :login_in?
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password'
     end
+
+    cookies[:email] = current_user&.email
   end
 
   def current_user
