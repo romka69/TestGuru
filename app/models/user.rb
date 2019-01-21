a774dd6b9d3bf64d8c2bb8cb4effb36a59d8a390
roman <romka69tver@gmail.com>
2019-01-21 23:40:21 +0300

    auth changed on 'has_secure_password'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,19 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
   has_many :created_tests, class_name: "Test", foreign_key: :author_id, dependent: :nullify
 
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+
   def test_by_level(level)
     tests.where(level: level)
   end
+
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
