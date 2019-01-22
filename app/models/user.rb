fd4708c9c170d750c4d8211eaddc43c64d4a544f
roman <romka69tver@gmail.com>
2019-01-22 23:24:35 +0300

    changed my auth to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
+
   VALID_EMAIL = /\A\w+@\w+\.\w+\z/.freeze
 
   has_many :test_passages, dependent: :destroy
@@ -7,14 +15,11 @@ class User < ApplicationRecord
 
   has_many :created_tests, class_name: "Test", foreign_key: :author_id, dependent: :nullify
 
-  validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: VALID_EMAIL
 
   def test_by_level(level)
     tests.where(level: level)
   end
-
-  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
