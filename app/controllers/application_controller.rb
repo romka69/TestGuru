8f09898cc0e60b92fd0ad6c2d99a0fce63195e5d
roman <romka69tver@gmail.com>
2019-01-23 22:27:53 +0300

    adds check admin params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def after_sign_in_path_for(user)
+    if user.admin?
+      admin_tests_path
+    else
+      super
+    end
+  end
 
 end
