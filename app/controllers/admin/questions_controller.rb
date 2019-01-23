1f17ae0338bace385d06ef170d849668552dcf4a
roman <romka69tver@gmail.com>
2019-01-23 22:25:12 +0300

    adds question and answer to admin

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,4 +1,5 @@
-class QuestionsController < ApplicationController
+class Admin::QuestionsController < Admin::BaseController
+
   before_action :find_test, only: %i[new create]
   before_action :find_question, only: %i[show edit update destroy]
 
@@ -18,7 +19,7 @@ class QuestionsController < ApplicationController
     @question = @test.questions.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to admin_question_path(@question)
     else
       render :new
     end
@@ -26,7 +27,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to @question
+      redirect_to admin_question_path(@question)
     else
       render :edit
     end
@@ -35,7 +36,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
 
-    redirect_to test_path(@question.test_id)
+    redirect_to admin_test_path(@question.test_id)
   end
 
   private
