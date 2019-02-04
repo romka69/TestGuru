286ff5c0776507c9d735fe5f55e2a0eed90db582
roman <romka69tver@gmail.com>
2019-02-04 20:15:16 +0300

    remove jquerry to vanila js

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,5 +1,11 @@
 document.addEventListener('turbolinks:load', function () {
-  $('.form-inline-link').on('click', formInlineLinkHandler);
+  var controls = document.querySelectorAll('.form-inline-link');
+
+  if (controls.length) {
+    for (var i = 0; i < controls.length; i++) {
+      controls[i].addEventListener('click', formInlineLinkHandler);
+    }
+  }
 
   var errors = document.querySelector('.resource-errors');
 
@@ -20,15 +26,16 @@ function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]');
   var editText = link.dataset.editText;
   var cancelText = link.dataset.cancelText;
-  var $testTitle = $('.test-title[data-test-id="' + testId + '"]');
-  var $formInline = $('.form-inline[data-test-id="' + testId + '"]');
+  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]');
+  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]');
 
-  $formInline.toggle();
-  $testTitle.toggle();
-
-  if ($formInline.is(':visible')) {
+  if (formInline.classList.contains('hide')) {
+    testTitle.classList.add('hide');
+    formInline.classList.remove('hide');
     link.textContent = cancelText;
   } else {
+    testTitle.classList.remove('hide');
+    formInline.classList.add('hide');
     link.textContent = editText;
   }
 }
