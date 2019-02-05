77ac63fbb6aa4a8228d81f13a9a41c99b5892a46
roman <romka69tver@gmail.com>
2019-02-05 20:44:25 +0300

    update to normal tests in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,42 @@
-backend, frontend = Category.create!(
-  [{ title: "Backend" }, { title: "Frontend" }]
+movie, geography = Category.create!(
+  [{ title: "Кино" }, { title: "География" }]
 )
 
-ruby = Test.create!(title: "Ruby", level: 1, category_id: backend.id, author_id: User.first.id)
-python = Test.create!(title: "Python", level: 2, category_id: backend.id, author_id: User.first.id)
-html = Test.create!(title: "HTML", level: 0, category_id: frontend.id, author_id: User.first.id)
-css = Test.create!(title: "CSS", level: 0, category_id: frontend.id, author_id: User.first.id)
+st = Test.create!(title: "Звездные Войны", level: 0, category_id: movie.id, author_id: User.first.id)
+jb = Test.create!(title: "Джеймс Бонд", level: 1, category_id: movie.id, author_id: User.first.id)
+ru = Test.create!(title: "Россия", level: 3, category_id: geography.id, author_id: User.first.id)
+usa = Test.create!(title: "США", level: 4, category_id: geography.id, author_id: User.first.id)
 
 questions = Question.create!(
-  [{ body: "Which command can download the library?", test_id: ruby.id },
-   { body: "Is it a scripting language?", test_id: python.id },
-   { body: "Was this language derived from XML?", test_id: html.id },
-   { body: "Is it a language that describes the look and feel of web pages?", test_id: css.id }]
+  [{ body: "Прошлое имя Дарта Вейдера?", test_id: st.id },
+   { body: "Как звали сестру Люка Скайуокера?", test_id: st.id },
+   { body: "Кто такие ситхи?", test_id: st.id },
+   { body: "Сколько оффициальных фильмов снято на текущий момент?", test_id: jb.id },
+   { body: "Как теперь называется остров в Тайланде, где проходили съемки фильма?", test_id: jb.id },
+   { body: "Столица России?", test_id: ru.id },
+   { body: "Где проходила олимпиада 2014?", test_id: ru.id },
+   { body: "Столица США?", test_id: usa.id },
+   { body: "Где пройдет олимпиада 2028?", test_id: usa.id }]
 )
 
 answers = Answer.create!(
-  [{ body: "Correct answer to Ruby", correct: true, question_id: questions[0].id },
-   { body: "Incorrect answer to Ruby", correct: false, question_id: questions[0].id },
-   { body: "Correct answer to Python", correct: true, question_id: questions[1].id },
-   { body: "Incorrect answer to Python", correct: false, question_id: questions[1].id },
-   { body: "Correct answer to HTML", correct: true, question_id: questions[2].id },
-   { body: "Incorrect answer to HTML", correct: false, question_id: questions[2].id },
-   { body: "Correct answer to CSS", correct: true, question_id: questions[3].id },
-   { body: "Incorrect answer to CSS", correct: false, question_id: questions[3].id }]
+  [{ body: "Энакин Скайуокер", correct: true, question_id: questions[0].id },
+   { body: "Джа-Джа Бинкс", correct: false, question_id: questions[0].id },
+   { body: "Хан Соло", correct: false, question_id: questions[0].id },
+   { body: "Палпатина", correct: false, question_id: questions[1].id },
+   { body: "Лея Органа", correct: true, question_id: questions[1].id },
+   { body: "Адепты темной стороны", correct: true, question_id: questions[2].id },
+   { body: "Адепты светлой стороны", correct: false, question_id: questions[2].id },
+   { body: "26 фильмов", correct: true, question_id: questions[3].id },
+   { body: "36 фильмов", correct: false, question_id: questions[3].id },
+   { body: "Остров Май Тай", correct: false, question_id: questions[4].id },
+   { body: "Остров Джеймс Бонда ", correct: true , question_id: questions[4].id },
+   { body: "Тверь", correct: false, question_id: questions[5].id },
+   { body: "Москва", correct: true, question_id: questions[5].id },
+   { body: "Сочи", correct: true, question_id: questions[6].id },
+   { body: "Тверь", correct: false, question_id: questions[6].id },
+   { body: "Нью-Йорк", correct: false, question_id: questions[7].id },
+   { body: "Вашингтон", correct: true, question_id: questions[7].id },
+   { body: "Сиэтл", correct: false, question_id: questions[8].id },
+   { body: "Лос Анжелес", correct: true, question_id: questions[8].id }]
 )
