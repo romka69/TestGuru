c552eb740bb119c466c68d8ffdd9e24df59f0375
roman <romka69tver@gmail.com>
2019-02-08 00:42:15 +0300

    add check timer in js side

---
+++ app/assets/javascripts/utilities/timer.js
@@ -0,0 +1,21 @@
+document.addEventListener('turbolinks:load', function () {
+  var timer = document.querySelector('.timer');
+
+  if (timer) {
+    var time_counter = timer.dataset.timer;
+    var test_passage_id = timer.dataset.id;
+
+    setInterval(function () {
+      if (time_counter > 0) {
+          time_counter -= 1;
+      } else {
+        alert('Time is over');
+        document.location.replace('/test_passages/' + test_passage_id + '/result');
+      }
+
+      var time_left = parseInt(time_counter / 60) + ':' + time_counter % 60;
+      timer.innerHTML = time_left;
+
+    }, 1000);
+  }
+});
